05c45c401714724ae8075a6708e7063a239b90ef
Ramya-Kavi <53213236+Ramya-Kavi@users.noreply.github.com>
2020-10-17 14:56:00 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+class Client{
+    public static void main(String[] args) throws Exception{
+        Socket s=new Socket("localhost",2222);
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str1="",str2="";
+        while(!str1.equals("stop")){
+            str1=br.readLine();
+            dout.writeUTF(str1);
+            dout.flush();
+            str2=din.readUTF();
+            System.out.println("Server says : "+str2);
+
+        }
+        dout.close();
+        s.close();
+    }
+
+}
---
+++ Server.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+
+class Server{
+    public static void main(String[] args) throws Exception{
+        ServerSocket ss=new ServerSocket(2222);
+        Socket s= ss.accept();
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str1="",str2="";
+        while(!str1.equals("stop")){
+            str1=din.readUTF();
+            System.out.println("Client says : "+str1);
+
+            str2=br.readLine();
+            dout.writeUTF(str2);
+            dout.flush();
+        }
+        din.close();
+        s.close();
+        ss.close();
+    }
+}
